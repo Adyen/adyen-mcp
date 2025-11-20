a4a93daa543f756fabd4a81bdde17537b00f7689
jilling <jilling.kingma@adyen.com>
2025-11-20 15:48:14 +0100

    Introduce Eslint/Prettier and run on project, create a ci job to validate

--- typescript/src/tools/tools.ts
+++ typescript/src/tools/tools.ts
@@ -1,12 +1,23 @@
-import { createPaymentLinkTool, getPaymentLinkTool, updatePaymentLinkTool } from "./paymentLinks";
-import { Tool } from "./types";
-import {cancelPaymentTool, refundPaymentTool} from "./modifications";
-import {createPaymentSessionTool, getPaymentMethodsTool, getPaymentSessionTool} from "./payments";
-import {getMerchantAccountsTool, listMerchantAccountsTool} from "./management";
-import { terminalTools } from "./terminals";
-import { createHostedOnboardingLinkTool } from "./legalEntityManagement/onboardingLinks";
-import { getLegalEntityTool } from "./legalEntityManagement/legalEntities";
-import { getAccountHolderTool } from "./configuration/accountHolders";
+import {
+  createPaymentLinkTool,
+  getPaymentLinkTool,
+  updatePaymentLinkTool,
+} from './paymentLinks/index.js';
+import { Tool } from './types.js';
+import { cancelPaymentTool, refundPaymentTool } from './modifications/index.js';
+import {
+  createPaymentSessionTool,
+  getPaymentMethodsTool,
+  getPaymentSessionTool,
+} from './payments/index.js';
+import {
+  getMerchantAccountsTool,
+  listMerchantAccountsTool,
+} from './management/index.js';
+import { terminalTools } from './terminals/index.js';
+import { createHostedOnboardingLinkTool } from './legalEntityManagement/onboardingLinks/index.js';
+import { getLegalEntityTool } from './legalEntityManagement/legalEntities/index.js';
+import { getAccountHolderTool } from './configuration/accountHolders/index.js';
 
 export const tools: Tool[] = [
   createPaymentLinkTool,
@@ -22,5 +33,5 @@ export const tools: Tool[] = [
   ...terminalTools,
   createHostedOnboardingLinkTool,
   getLegalEntityTool,
-  getAccountHolderTool
+  getAccountHolderTool,
 ];
