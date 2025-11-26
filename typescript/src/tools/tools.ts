b5b6f158e8b97e8e5223d0dddc964c4d16f0136c
jilling <jilling.kingma@adyen.com>
2025-11-26 11:25:23 +0100

    Introduce Eslint/Prettier and run on project, create a ci job to validate and cleanup up tool directory structure

--- typescript/src/tools/tools.ts
+++ typescript/src/tools/tools.ts
@@ -1,18 +1,32 @@
-import { createPaymentLinkTool, getPaymentLinkTool, updatePaymentLinkTool } from "./paymentLinks";
-import { Tool } from "./types";
-import {cancelPaymentTool, refundPaymentTool} from "./modifications";
-import {createPaymentSessionTool, getPaymentMethodsTool, getPaymentSessionTool} from "./payments";
-import {getMerchantAccountsTool, listMerchantAccountsTool} from "./management";
-import { terminalTools } from "./terminals";
-import { createHostedOnboardingLinkTool } from "./legalEntityManagement/onboardingLinks";
-import { getLegalEntityTool } from "./legalEntityManagement/legalEntities";
-import { getAccountHolderTool } from "./configuration/accountHolders";
 import {
-    getCompanyWebhookTool,
-    getMerchantWebhookTool,
-    listAllCompanyWebhooksTool,
-    listAllMerchantWebhooksTool,
-} from "./webhooks";
+  createPaymentLinkTool,
+  getPaymentLinkTool,
+  updatePaymentLinkTool,
+} from './checkout/paymentLinks/index.js';
+import { Tool } from './types.js';
+import {
+  cancelPaymentTool,
+  refundPaymentTool,
+} from './checkout/modifications/index.js';
+import {
+  createPaymentSessionTool,
+  getPaymentMethodsTool,
+  getPaymentSessionTool,
+} from './checkout/payments/index.js';
+import {
+  getMerchantAccountsTool,
+  listMerchantAccountsTool,
+} from './management/accounts/index.js';
+import { terminalTools } from './management/terminals/index.js';
+import { createHostedOnboardingLinkTool } from './legalEntityManagement/onboardingLinks/index.js';
+import { getLegalEntityTool } from './legalEntityManagement/legalEntities/index.js';
+import { getAccountHolderTool } from './configuration/accountHolders/index.js';
+import {
+  getCompanyWebhookTool,
+  getMerchantWebhookTool,
+  listAllCompanyWebhooksTool,
+  listAllMerchantWebhooksTool,
+} from './management/webhooks/index.js';
 
 export const tools: Tool[] = [
   createPaymentLinkTool,
@@ -32,5 +46,5 @@ export const tools: Tool[] = [
   listAllCompanyWebhooksTool,
   getCompanyWebhookTool,
   listAllMerchantWebhooksTool,
-  getMerchantWebhookTool
+  getMerchantWebhookTool,
 ];
